937bcb4d75cbc944126bf8f3e4190c5e38eb0066
Nguyen Dinh Duan <duan.nd@senseinfosys.com>
2018-10-16 16:57:21 +0700

    -- add spring boot mvc project

---
+++ spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/SpringMvcApplication.java
@@ -0,0 +1,16 @@
+package com.duannd.springboot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created By ngdduan@gmail.com at 10/16/2018 9:48 AM
+ */
+@SpringBootApplication
+public class SpringMvcApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(SpringMvcApplication.class, args);
+    }
+
+}
