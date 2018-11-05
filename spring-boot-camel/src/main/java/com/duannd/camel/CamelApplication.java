50ee261efec6fff04fb6857b14a05c3c85988c11
Nguyen Dinh Duan <duan.nd@senseinfosys.com>
2018-11-05 17:59:30 +0700

    add some project

---
+++ spring-boot-camel/src/main/java/com/duannd/camel/CamelApplication.java
@@ -0,0 +1,16 @@
+package com.duannd.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created By duan.nd@senseinfosys.com at 10/29/2018 1:36 PM
+ */
+@SpringBootApplication
+public class CamelApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(CamelApplication.class, args);
+    }
+
+}
