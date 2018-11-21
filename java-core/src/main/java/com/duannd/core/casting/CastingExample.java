d267ea178d24b9a7c2a1acd97a9a0a6f3370e222
Nguyen Dinh Duan <duan.nd@senseinfosys.com>
2018-11-21 17:42:52 +0700

    Add tutorials for Casting and Generics

---
+++ java-core/src/main/java/com/duannd/core/casting/CastingExample.java
@@ -0,0 +1,7 @@
+package com.duannd.core.casting;
+
+/**
+ * Created By duan.nd@senseinfosys.com at 11/21/2018 9:22 AM
+ */
+public class CastingExample {
+}
---
+++ java-core/src/main/java/com/duannd/core/models/Animals.java
@@ -0,0 +1,7 @@
+package com.duannd.core.models;
+
+/**
+ * Created By duan.nd@senseinfosys.com at 11/21/2018 10:36 AM
+ */
+public class Animals {
+}
