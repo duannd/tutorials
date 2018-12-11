0680862c01e23cd274992084f148d458d29b2e8d
Nguyen Dinh Duan <duan.nd@senseinfosys.com>
2018-12-11 10:32:44 +0700

    update date-time tutorials

--- java8/src/main/java/com/duannd/java8/datetime/InstantExample.java
+++ java8/src/main/java/com/duannd/java8/datetime/InstantExample.java
@@ -1,7 +1,23 @@
 package com.duannd.java8.datetime;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 /**
- * Created By duan.nd@senseinfosys.com at 12/10/2018 4:15 PM
+ * Created By ngdduan@gmail.com at 12/10/2018 4:15 PM
  */
 public class InstantExample {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm.ss.SSS");
+
+    public static void main(String[] args) {
+        Instant utcInstant = Instant.now(Clock.system(ZoneId.of("+07:00")));
+        System.out.println("UTC time: " + utcInstant.atZone(ZoneOffset.UTC).format(FORMATTER));
+        // Convert from UTC to localDateTime
+        System.out.println("HCM time: " + utcInstant.atZone(ZoneId.of("+07:00")).format(FORMATTER));
+    }
+
 }
--- java8/src/main/java/com/duannd/java8/datetime/LocalDateExample.java
+++ java8/src/main/java/com/duannd/java8/datetime/LocalDateExample.java
@@ -3,10 +3,10 @@ package com.duannd.java8.datetime;
 /**
  * Created By ngdduan@gmail.com at 12/10/2018 3:43 PM
  */
-public class DateTimeExample {
+public class LocalDateExample {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
