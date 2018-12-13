443d1f988f7f27411ec9bf1f38a7b8e0b84b6507
Nguyen Dinh Duan <duan.nd@senseinfosys.com>
2018-12-13 18:17:15 +0700

    add base type tutorials

---
+++ java-core/src/main/java/com/duannd/core/models/NewInterfaces.java
@@ -0,0 +1,7 @@
+package com.duannd.core.models;
+
+/**
+ * Created By duan.nd@senseinfosys.com at 12/12/2018 1:48 PM
+ */
+public interface NewInterfaces {
+}
---
+++ spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/models/NewTest.java
@@ -0,0 +1,9 @@
+package com.duannd.springboot.mvc.models;
+
+import com.duannd.core.models.NewInterfaces;
+
+/**
+ * Created By duan.nd@senseinfosys.com at 12/12/2018 1:49 PM
+ */
+public class NewTest implements NewInterfaces {
+}
