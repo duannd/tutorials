3048135ef9039e33280b2f7e3ef3a1f80897d82a
duan.nguyen <duan.nguyen@mservice.com.vn>
2018-12-31 22:10:41 +0700

    Add project and logback for java core

--- spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/models/NewTest.java
+++ spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/models/NewTest.java
@@ -1,9 +1,7 @@
 package com.duannd.springboot.mvc.models;
 
-import com.duannd.core.models.NewInterfaces;
-
 /**
  * Created By duan.nd@senseinfosys.com at 12/12/2018 1:49 PM
  */
-public class NewTest implements NewInterfaces {
+public class NewTest {
 }
