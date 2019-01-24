2098d207af9e196097f2af864e7da3797cb49d37
duan.nguyen <duan.nguyen@mservice.com.vn>
2019-01-24 07:24:32 +0700

    Add main function

--- data-structure-algorithms/src/main/java/com/duannd/algorithms/AlgorithmsMain.java
+++ data-structure-algorithms/src/main/java/com/duannd/algorithms/AlgorithmsMain.java
@@ -3,10 +3,10 @@ package com.duannd.algorithms;
 /**
  * Created By ngdduan@gmail.com at 2019-01-23 07:31
  */
-public class AlogrithmsMain {
+public class AlgorithmsMain {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
--- data-structure-algorithms/src/main/java/com/duannd/datastructures/DataStructuresMain.java
+++ data-structure-algorithms/src/main/java/com/duannd/datastructures/DataStructuresMain.java
@@ -4,4 +4,9 @@ package com.duannd.datastructures;
  * Created By ngdduan@gmail.com at 2019-01-23 07:32
  */
 public class DataStructuresMain {
+
+    public static void main(String[] args) {
+
+    }
+
 }
