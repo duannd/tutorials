a9642c6aa6eae2c0e36902d74224633b8e61491d
Duan Nguyen <ngdduan@gmail.com>
2019-02-16 12:49:32 +0700

    Change name for bean scope class

--- spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/SpringMvcApplication.java
+++ spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/SpringMvcApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
