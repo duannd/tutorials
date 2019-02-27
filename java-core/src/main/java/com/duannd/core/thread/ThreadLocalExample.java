f13d8cb75b7755e3af2c8fe67263513f7a24252f
Duan Nguyen <ngdduan@gmail.com>
2019-02-27 07:24:13 +0700

    commit thread safe

---
+++ java-core/src/main/java/com/duannd/core/thread/ThreadLocalExample.java
@@ -0,0 +1,7 @@
+package com.duannd.core.thread;
+
+/**
+ * Created By ngdduan@gmail.com at 2019-02-22 00:47
+ */
+public class ThreadLocalExample {
+}
---
+++ java-core/src/main/java/com/duannd/core/thread/ThreadSafetyExample.java
@@ -0,0 +1,7 @@
+package com.duannd.core.thread;
+
+/**
+ * Created By ngdduan@gmail.com at 2019-02-22 00:06
+ */
+public class ThreadSafetyExample {
+}
