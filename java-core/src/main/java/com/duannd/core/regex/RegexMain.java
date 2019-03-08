ae135320db816b7a3e462a505042ebd5dc5243eb
Nguyen Dinh Duan <duan.nd@senseinfosys.com>
2019-03-08 17:32:13 +0700

    Add Regex example

--- java-core/src/main/java/com/duannd/core/regex/RegexMain.java
+++ java-core/src/main/java/com/duannd/core/regex/RegexMain.java
@@ -1,7 +1,21 @@
 package com.duannd.core.regex;
 
+import java.util.regex.Pattern;
+
 /**
  * Created By duan.nd@senseinfosys.com at 3/8/2019 2:23 PM
+ * @see "https://www.journaldev.com/634/regular-expression-in-java-regex-example"
  */
 public class RegexMain {
+
+    public static void main(String[] args) {
+        var pattern = Pattern.compile("default_[a-z]+_w1");
+        System.out.println("default_cus_w1: " + pattern.matcher("default_cus_w1").matches());
+        System.out.println("a_cus_w1: " + pattern.matcher("a_cus_w1").matches());
+        System.out.println("default_cus_w: " + pattern.matcher("default_cus_w").matches());
+        System.out.println("_cus_: " + pattern.matcher("_cus_").matches());
+        System.out.println("a: " + pattern.matcher("a").matches());
+        System.out.println("" + (32000000 / 23265));
+    }
+
 }
