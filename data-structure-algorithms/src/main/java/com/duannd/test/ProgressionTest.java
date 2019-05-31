201098eaae048917422659296178db913a65464a
duan.nguyen <duan.nguyen@formos.com>
2019-05-31 11:55:13 +0700

    Commit complete OOP test

--- data-structure-algorithms/src/main/java/com/duannd/test/ProgressionTest.java
+++ data-structure-algorithms/src/main/java/com/duannd/test/ProgressionTest.java
@@ -2,6 +2,8 @@ package com.duannd.test;
 
 import com.duannd.models.Progression;
 import com.duannd.models.sub.ArithmeticProgression;
+import com.duannd.models.sub.FibonacciProgression;
+import com.duannd.models.sub.GeometricProgression;
 
 public class ProgressionTest {
 
@@ -9,7 +11,7 @@ public class ProgressionTest {
         Progression prog;
         // test ArithmeticProgression
         System.out.print("Arithmetic progression with default increment: ");
-        prog = new ArithmeticProgression( );
+        prog = new ArithmeticProgression();
         prog.printProgression(10);
         System.out.print("Arithmetic progression with increment 5: ");
         prog = new ArithmeticProgression(5);
@@ -18,6 +20,19 @@ public class ProgressionTest {
         prog = new ArithmeticProgression(5, 2);
         prog.printProgression(10);
         // Test GeometricProgression
+        System.out.print("Geometric progression with default base: ");
+        prog = new GeometricProgression();
+        prog.printProgression(10);
+        System.out.print("Geometric progression with base 3: ");
+        prog = new GeometricProgression(3);
+        prog.printProgression(10);
+        // test FibonacciProgression
+        System.out.print("Fibonacci progression with default start values: ");
+        prog = new FibonacciProgression();
+        prog.printProgression(10);
+        System.out.print("Fibonacci progression with start values 4 and 6: ");
+        prog = new FibonacciProgression(4, 6);
+        prog.printProgression(8);
     }
 
 }
