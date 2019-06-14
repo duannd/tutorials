bd141e3a5bd0fc65fcc923a06461eece76d605ea
duan.nguyen <duan.nguyen@formos.com>
2019-06-14 13:13:39 +0700

    Add consumes and produces examples in Spring MVC module. Add more plain-text

--- spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/controllers/StudentController.java
+++ spring-boot-mvc/src/main/java/com/duannd/springboot/mvc/controllers/StudentController.java
@@ -90,4 +90,14 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(student);
     }
 
+    @PostMapping(value = "consumes-produces", consumes = "application/vnd.api.v2+json",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity consumesDiffProduces(@RequestBody CreateStudentRequest request,
+                                               @RequestParam(required = false) String other) throws JsonProcessingException {
+        var student = new CreateStudentResponse(request.getName());
+        CACHE_RESPONSE.put(student.getId(), student);
+        // var studentString = this.objectMapper.writeValueAsString(student);
+        return ResponseEntity.status(HttpStatus.CREATED).body(student);
+    }
+
 }
