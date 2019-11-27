69857ce456fae2037ca35031fbd8ceb5915b0ae8
duan.nguyen <duan.nguyen@formos.com>
2019-11-27 11:27:38 +0700

    Java 11: Local variable syntax interface.

--- java11/src/main/java/com/duannd/java11/LocalVariableSyntaxMain.java
+++ java11/src/main/java/com/duannd/java11/LocalVariableSyntaxMain.java
@@ -1,15 +1,41 @@
 package com.duannd.java11;
 
+import lombok.*;
+
+import java.util.function.Function;
+
 public class LocalVariableSyntaxMain {
 
     public static void main(String[] args) {
         System.out.println("----- Local Variable Syntax -----");
         localVariableSyntax();
+        System.out.println("----- Lambda & FunctionalInterface -----");
+        getFullName();
     }
 
     private static void localVariableSyntax() {
         ILocalVariableSyntax syntax = (s1, s2) -> s1 + s2 + 1;
         System.out.println(syntax.plusOne(5, 9));
+    }
+
+    private static void getFullName() {
+        Function<String, Student> function = (@NonNull var firstName) -> {
+            String fullName = "Hello, " + firstName;
+            return new Student(fullName);
+        };
+        System.out.println(function.apply("DuanND"));
+        System.out.println(function.apply(null));
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    private static class Student {
+
+        private String fullName;
+
     }
 
 }
