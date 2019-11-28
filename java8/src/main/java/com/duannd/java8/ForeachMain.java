6dcf2b77e75e4f1ef195c83849ee1a42a6895bf2
duan.nguyen <duan.nguyen@formos.com>
2019-11-28 09:58:22 +0700

    Java 08: Update foreach to throw java.util.ConcurrentModificationException

--- java8/src/main/java/com/duannd/java8/ForeachMain.java
+++ java8/src/main/java/com/duannd/java8/ForeachMain.java
@@ -1,5 +1,6 @@
 package com.duannd.java8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,21 @@ public class ForeachMain {
     public static void main(String[] args) {
         System.out.println("----- Java 8 foreach -----");
         foreach();
+        System.out.println("----- Foreach & change size of Collections -----");
+        foreachAndChangeSizeOfCollection();
     }
 
     private static void foreach() {
         List<String> names = Arrays.asList("Duan", "Gate", "Trump", "Hang");
         names.forEach(System.out::println);
+    }
+
+    private static void foreachAndChangeSizeOfCollection() {
+        List<String> names = new ArrayList<>(Arrays.asList("Duan", "Gate", "Trump", "Hang"));
+        names.forEach(s -> {
+            System.out.println(s);
+            names.add("Test"); // throw java.util.ConcurrentModificationException
+        });
     }
 
 }
